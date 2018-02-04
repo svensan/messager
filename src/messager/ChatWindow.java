cd2abaf068307454693978ed93311756839e9799
Max Oliveberg <maxoliveberg@MacBook-Pro.local>
2018-02-04 15:21:02 +0100

    flytta runt

--- src/messager/ChatWindow.java
+++ src/messager/ChatWindow.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projekt_gui;
+package messager;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
--- src/messager/Projekt_GUI.java
+++ src/messager/Projekt_GUI.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projekt_gui;
+package messager;
+
+import messager.MainMenu;
+import messager.ChatWindow;
 
 /**
  *
